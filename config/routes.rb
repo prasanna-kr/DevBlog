098b587665e9c905e88b9e7143e3fdf137a24487
prasanna <prasanna.k.r@accenture.com>
2022-01-18 19:03:56 +0530

    Dev Blog

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :users
+  resources :articles do
+    resources :comments
+  end
+  get 'home/index'
+  root "articles#index"
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
